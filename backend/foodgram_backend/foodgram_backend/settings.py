28230fa031b715d0adfa248b83aa0ff6f4d1a34b
Bogatyrev Roman <r.pittchai@yandex.ru>
2023-06-28 16:50:27 +0300

    first/280623

---
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -0,0 +1,96 @@
+# Добавить игнор для флейк
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Убрать ключ
+SECRET_KEY = 'django-insecure-+##zzv^)g-au9#tc1+1&t!21v0_ynwuh!p78ht&f*nidhu4qrj'
+
+# Выставить True
+DEBUG = False
+
+# Добавить разрешённые хосты
+ALLOWED_HOSTS = []
+
+# Добавить приложения
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'foodgram_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
+
+# Заменить на постгресс.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Заменить на русский и поставить часовой пояс Москвы
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Указать все папки со статикой
+
+STATIC_URL = 'static/'
+
+#  Добавить пермишены для аутоинтификации и не забыть про джоссер
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
