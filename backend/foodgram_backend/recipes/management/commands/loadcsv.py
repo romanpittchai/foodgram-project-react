175ecf941f6a260201697bbc0dedc9848209ad18
Bogatyrev Roman <r.pittchai@yandex.ru>
2023-07-12 13:15:57 +0300

    users

--- backend/foodgram_backend/recipes/management/commands/loadcsv.py
+++ backend/foodgram_backend/recipes/management/commands/loadcsv.py
@@ -6,11 +6,9 @@ from django.core.management.base import BaseCommand
 
 from foodgram_backend.settings import STATIC_CSV_JSON_FILES_DIRS
 from recipes.models import Ingredient, Tag
-from users.models import User
 
 DICT_FILE = {
     Ingredient: 'ingredients.csv',
-    User: 'users.csv',
     Tag: 'tags.csv',
 }
 
