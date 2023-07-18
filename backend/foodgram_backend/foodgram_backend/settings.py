1e7ca5ef4935dd7d4f55f7ab0ee860c7b2e9283d
Bogatyrev Roman <r.pittchai@yandex.ru>
2023-07-18 21:38:01 +0300

    180723

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
